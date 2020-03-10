c7fa92038961b8bdcf84b00de31e57b0b1c83d26
Robert King <robert.o.king@hotmail.co.uk>
2020-03-10 15:30:00 +0100

    CRUD in Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all tasks
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+  # Create new task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # Read one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Update a task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # Delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
