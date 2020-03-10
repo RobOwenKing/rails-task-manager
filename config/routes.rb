04865e19d6f67a751d809c7237bebe0d07535dd3
Robert King <robert.o.king@hotmail.co.uk>
2020-03-10 15:41:39 +0100

    Refactoring, DRYing code and adding .persisted? to new/edit task form

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Read all tasks
-  get '/tasks', to: 'tasks#index', as: :tasks
+  resources :tasks
 
-  # Create new task
-  get '/tasks/new', to: 'tasks#new', as: :new_task
-  post '/tasks', to: 'tasks#create'
+  # # Read all tasks
+  # get '/tasks', to: 'tasks#index', as: :tasks
 
-  # Read one task
-  get '/tasks/:id', to: 'tasks#show', as: :task
+  # # Create new task
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
+  # post '/tasks', to: 'tasks#create'
 
-  # Update a task
-  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch '/tasks/:id', to: 'tasks#update'
+  # # Read one task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
 
-  # Delete a task
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # # Update a task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # # Delete a task
+  # delete '/tasks/:id', to: 'tasks#destroy'
 end
